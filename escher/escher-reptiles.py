959488672ba6efd14a259e43c3c88ad100e46669
Karsten Wolf <karstenwo@web.de>
2013-09-05 10:29:18 +0200

    Added Escher demos.

---
+++ escher/escher-reptiles.py
@@ -0,0 +1,361 @@
+background(0.8)
+size(1440,900)
+import math
+import pprint
+
+kwdbg = 0
+
+r30 = math.radians( 30)
+c30 = math.cos( r30 )
+
+def angle(a, b):
+    #calculate the angle between two points
+    x0, y0 = a
+    x1, y1 = b
+    a = math.degrees( math.atan2(y1-y0, x1-x0) )
+    return a
+
+
+def distance( a, b ):
+    #calculate the distance between two points
+    dx = b[0] - a[0]
+    dy = b[1] - a[1]
+    return math.sqrt(dx * dx + dy * dy)
+
+
+# the reptile consists of 3 different segments; each used twice
+seg1 = (
+    ( 0.0,  0.0),
+    ( 2.2,  2.2),
+    ( 4.0,  2.2),
+    ( 6.3, -0.5),
+    ( 5.0, -3.0),
+    ( 5.8, -3.8),
+    ( 7.5, -3.7),
+    ( 7.8, -3.0),
+    ( 7.0, -2.0),
+    ( 8.0,  0.0))
+
+seg2 = (
+    ( 0.0,  0.0),
+    (-1.0,  2.0),
+    ( 1.0,  2.5),
+    ( 3.8,  1.3),
+    ( 4.5,  0.0),
+    ( 4.8, -3.0),
+    ( 3.0, -4.8),
+    ( 6.8, -3.2),
+    ( 8.0, -0.8),
+    ( 8.0,  0.0))
+
+seg3 = (
+    ( 0.0,  0.0),
+    ( 1.0,  2.5),
+    ( 0.7,  4.8),
+    ( 2.8,  4.0),
+    ( 2.8,  2.9),
+    ( 2.1,  0.25),
+    ( 4.0, -0.5),
+    ( 5.3, -1.75),
+    ( 8.0,  0.0))
+
+
+def getsegment( segment, start, end, reverse):
+    # get a list of coordinates for segment adapted between start and end
+
+    # All the segments for one reptile need to be drawn in one go, so
+    # instead of rotating them, they are drawn strictly clockwise.
+    
+    # to achieve that, half of the segments need to be reversed.
+
+    if 0: #kwdbg:
+        segment = ( (0,0), (4,1), (4,-1), (8,0))
+    seg = segment[:]
+
+    if reverse:
+        seg = []
+        for i in segment:
+            x,y = i
+            # get this ugly constant out
+            seg.append( (8.0-x, -y) )
+        seg.reverse()
+
+    a = angle( start, end )
+    s = distance( start, end )
+    r = math.radians( a )
+    cosr = math.cos(r)
+    sinr = math.sin(r)
+
+    #print "scale:", s
+    #print "angle:", a
+    result = []
+
+    for p in seg:
+        px, py = p
+        if 1: # reverse:
+            # segments were constructed in an y-switched coordinate system; correct it
+            py = py * -1.0
+            #px = px * -1.0
+
+        # scale
+        px *= s/8
+        py *= s/8
+        
+        # rotate
+        px2 = px * cosr - py * sinr
+        py2 = px * sinr + py * cosr
+
+        # translate
+        px2 += start[0]
+        py2 += start[1]
+
+        result.append( (px2, py2) )
+
+    return result
+
+
+def verticalhexagon(x, y, radius):
+    # flat top/bottom
+    height = radius * c30
+    halfradius = radius / 2.0
+
+    return [
+        ( x + radius    ,  y),
+        ( x + halfradius  , y + height),
+        ( x - halfradius  , y + height),
+        ( x - radius      , y),
+        ( x - halfradius  , y - height),
+        ( x + halfradius  , y - height)]
+
+def veroffsets(x, y, radius, shift):
+    shifts = {
+         0: (2,4,2,4,2,4),
+         1: (5,3,5,3,5,3),
+         2: (4,0,4,0,4,0),
+         3: (1,5,1,5,1,5),
+         4: (0,2,0,2,0,2),
+         5: (3,1,3,1,3,1)}
+    shifts = {
+         0: (2,4,2,4,2,4),
+         1: (3,5,3,5,3,5),
+         2: (4,0,4,0,4,0),
+         3: (5,1,5,1,5,1),
+         4: (0,2,0,2,0,2),
+         5: (1,3,1,3,1,3)}
+             
+    height = radius * c30
+    halfradius = radius / 2.0
+    side = 1.5 * radius
+    up = 2 * height
+
+    s = shifts[shift]
+    return [
+        ( x + side , y - height, s[0]),
+        ( x + side , y + height, s[1]),
+        ( x        , y + up,     s[2]),
+        ( x - side , y + height, s[3]),
+        ( x - side , y - height, s[4]),
+        ( x        , y - up,     s[5])]
+
+def horizontalhexagon(x, y, radius):
+    # flat left/right
+    height = radius * c30
+    halfradius = radius / 2.0
+    return [
+        ( x + 0     , y - radius),
+        ( x + height, y - halfradius),
+        ( x + height, y + halfradius),
+        ( x + 0     , y + radius),
+        (x - height , y + halfradius),
+        (x - height , y - halfradius)]
+
+def horoffsets(x, y, radius, shift):
+    shifts = {
+         0: (4,2,4,2,4,2),
+         1: (3,5,3,5,3,5),
+         2: (0,4,0,4,0,4),
+         3: (5,1,5,1,5,1),
+         4: (2,0,2,0,2,0),
+         5: (1,3,1,3,1,3)}
+             
+    height = radius * c30
+    halfradius = radius / 2.0
+    
+    side = 2*height
+    up = 1.5*radius
+    corner = height
+    s = shifts[shift]
+    return [
+        ( x + height , y - up,  s[0]),
+        ( x + side   , y     ,  s[1]),
+        ( x + height , y + up,  s[2]),
+
+        ( x - height , y + up,  s[3]),
+        ( x - side   , y     ,  s[4]),
+        ( x - height , y - up,  s[5])]
+
+
+def reptile(x, y, radius, rot, shift, vert):
+
+    if vert:
+        h = verticalhexagon( x, y, radius )
+    else:
+        h = horizontalhexagon( x, y, radius )
+    
+
+    # pprint.pprint(h[0])
+    s = shift
+    while s > 0:
+        s -= 1
+        t = h.pop()
+        h.insert(0, t)
+        # pprint.pprint(h[0])
+    
+    hseg = [
+        (h[0], h[1], seg1, 1),
+        (h[1], h[2], seg1, 0),
+        (h[2], h[3], seg3, 1),
+        (h[3], h[4], seg3, 0),
+        (h[4], h[5], seg2, 1),
+        (h[5], h[0], seg2, 0)]
+
+    result = []
+
+    for part in hseg:
+        p1 = part[0]
+        p2 = part[1]
+        seg = part[2]
+        rev = part[3]
+        
+        s = getsegment( seg, p1, p2, rev)
+        # s = [ p1, p2 ]
+        result.extend( s )
+
+    # debug stuff
+    if kwdbg:
+        push()
+        nofill()
+        stroke(0,0,1,0.5)
+        autoclosepath(1)
+        beginpath()
+        hl = list(h)
+        p0 = hl.pop(0)
+        textmarker((x,y-25), str(shift), 25)
+
+        moveto( *p0 )
+        #textmarker(p0, "h1", 20)
+        nofill()
+        i=2
+        for p in hl:
+            lineto( *p )
+            # textmarker(p, "h"+str(i), 20)
+            nofill()
+            i += 1
+        endpath(1)
+        pop()
+    return result
+
+def textmarker(p, s, fsize):
+    #return
+    push()
+    px, py = p
+    fill(0.66, 0.66, 0.66, 0.66)
+    if fsize > 25:
+        fill(0.33, 0.33, 0.33, 0.66)
+    fontsize(fsize)
+    w = textwidth( s ) / 2.0
+    h = textheight(s)
+    text(s, px-w, py+h+1)
+    pop()
+
+def drawlines( lines, close=False):
+    # return
+    start = lines[0]
+    
+    autoclosepath(False)
+    if close:
+        autoclosepath(True)
+    beginpath()
+    moveto( *start )
+    last = start
+    i = 0
+    # textmarker(start, str(i), 8)
+    nofill()
+    if close:
+        fill( random(), random(), random(), random()/4.0)
+    for l in lines[1:]:
+        i += 1
+        if l != last:
+            lineto( *l )
+            #textmarker( l, str(i),8)
+            #nofill()
+        last = l
+        
+    p = endpath()
+    stroke(0)
+    #nofill()
+    drawpath( p )
+
+nofill()
+stroke(0)
+
+def inrect( px,py,rd,t,l,b,r):
+    if px+rd < l or px-rd > r:
+        return False
+    if py+rd < t or py-rd > b:
+        return False
+    return True
+
+
+def hexfill( sx,sy,s,rd,vert,t,l,b,r):
+    result = [ (sx,sy,s) ]
+    start = hashcoordinates( sx,sy )
+    locations = { (start): s }
+    
+    hexcoords = generate(sx,sy,rd,s,vert)
+
+    while len(hexcoords) > 0:
+        
+        tmp = hexcoords.pop(0)
+        px,py,ps = tmp
+        
+        if inrect(px,py,rd,t,l,b,r):
+            cx,cy = hashcoordinates(px,py)
+            if not (cx,cy) in locations:
+                locations[ (cx,cy) ] = None
+                result.append( tmp )
+                tmp = generate(px,py,rd,ps,vert)
+                hexcoords.extend( tmp )
+    return result
+
+def hashcoordinates( x,y,res=2 ):
+    x = int( round(x,res) * ( 10**res ))
+    y = int( round(y,res) * ( 10**res ))
+    return (x,y)
+
+def generate(x,y,r,s, horver=True):
+    """
+    """
+    result = [ (x,y,s) ]
+    offsets = horoffsets
+    if horver:
+        offsets = veroffsets
+    for i in offsets(x, y, r, s):
+        result.append( i )
+    return result
+
+def tileReptiles( x,y, orientation, radius, hexType, *rectangle):
+
+    pprint.pprint( ( x,y, orientation, radius, hexType, rectangle) )
+
+    items = hexfill( x,y, orientation, radius, hexType, *rectangle)
+    print "#items:",len(items)
+    for item in items:
+        px, py, ps = item
+        rd = reptile( px, py, radius, 0, ps, hexType)
+        drawlines( rd, 1 )
+
+#tileReptiles(720,450,0,120,True,0,0,900,1440)
+tileReptiles(720,450,0,60,True,0,0,900,1440)
+#tileReptiles(720,450,0,30,True,0,0,900,1440)
+
---
+++ escher/squarelimit.py
@@ -0,0 +1,578 @@
+background( None )
+size(1100,900)
+import pprint
+pp = pprint.pprint
+
+import math
+
+"""
+
+"""
+
+
+# scale factor for next smaller picture
+smaller = 1 / math.sqrt(2)
+
+
+# global counter
+no_of_pictures = 0
+
+# unused
+types = {
+    1: "line",
+    2: "linesegments",
+    3: "curvesegments"}
+
+# debug options
+# draw a rectangle around picture
+dbg_frame = False
+
+# draw a circle around points
+dbg_points = False
+
+# draw a circle around startpoints
+dbg_starts = False
+
+# switch off content rendering
+dbg_NOCONTENT = False
+
+# scales content to 85%
+dbg_smallerscale = False
+
+# guess what, gives pictures a twist
+dbg_randomtwist = False
+
+# set pictures apart by self.size / offsetpart
+dbg_offset = False
+
+# 1/25.0 -> 4% offset
+offsetpart = 25.0
+
+dbg_any = (   dbg_offset
+           or dbg_smallerscale
+           or dbg_NOCONTENT
+           or dbg_starts
+           or dbg_points
+           or dbg_frame
+           or dbg_randomtwist)
+
+# color for debug marks
+mark_color = color(1.0, 0.0, 0.0, 1.0)
+
+# std line color
+line_color = color(0.0, 0.0, 0.0, 1.0)
+
+pictlinewidth = 1.0
+framelinewidth = 0.3
+
+
+class Segment(object):
+    """
+    Hold a line or polygon.
+
+    type - what to draw with point list
+        1: a line
+        2: a polygon
+        3: a curvesegment
+
+    points - list of x,y tuples
+
+
+    Currently only polygons are drawn
+    """
+
+    def __init__(self, type_, points):
+        self.type = type_
+        self.points = points
+
+    def draw(self, context):
+        if self.type == 1:
+            """
+            A single line.
+            """
+            pass
+
+        elif self.type == 2:
+            """
+            a list of points drawn as a polygon. If first and last point are the
+            same, last point is popped and autoclosepath is called.
+            """
+            push()
+
+            # stupid experiment... funny results
+            # seems like it was the wrong place...
+            if False: # dbg_smallerscale:
+                transform(CENTER)
+                scale(0.85)
+
+            L = list(self.points) # [:]
+            n = len(self.points)
+
+            # check for a closed polygon
+            if L[0] == L[-1]:
+                close = True
+                L.pop()
+            else:
+                close = False
+            autoclosepath( close )
+
+            beginpath()
+
+            if dbg_frame or dbg_points or dbg_starts:
+                # draw a circle at my origin
+                strokewidth(framelinewidth)
+                stroke( mark_color )
+                oval(-2, -2, 4, 4)
+                stroke( line_color )
+                strokewidth(pictlinewidth)
+
+            for i,p in enumerate(L):
+                if i == 0:
+                    # first point
+                    moveto( p[0], p[1] )
+                    # last = p
+                else:
+                    lineto(p[0], p[1])
+                    if dbg_points:
+                        strokewidth(framelinewidth)
+                        oval(p[0]-1, p[1]-1, 2, 2)
+                        strokewidth(pictlinewidth)
+                    # last = p
+
+            if not dbg_NOCONTENT:
+                path = endpath(draw=True)
+                #drawpath( path )
+
+            if False: #dbg_smallerscale:
+                transform(CORNER)
+                scale(1.15)
+
+            pop()
+        elif self.type == 3:
+            pass
+
+
+class Picture(object):
+    """A list of Segments make a Picture.
+    """
+
+    def __init__(self, size_, segments):
+        # extent
+        self.size = size_
+
+        # segmentlist
+        self.segments = segments
+
+    def draw_NBContext(self, c=None):
+        if dbg_frame:
+            push()
+            strokewidth(framelinewidth)
+            x = self.size[0]
+            y = self.size[1]
+            if True:
+                rect(0,0,self.size[0], self.size[1])
+            else:
+                line(0,0, x,0)
+                line(x,0, 0,y)
+                line(0,y, 0,0)
+            strokewidth(pictlinewidth)
+            pop()
+
+        if dbg_randomtwist:
+            r = random(-15.0, 15.0)
+            rotate(r)
+
+        if dbg_smallerscale:
+            #transform(CENTER)
+            scale(0.85)
+
+        for i, s in enumerate(self.segments):
+            if dbg_frame:
+                # mark origin of picture
+                if i == 0:
+                    # rect(-2,-2,4,4)
+                    pass
+            push()
+            s.draw(None)
+            pop()
+    draw = draw_NBContext
+
+
+    def draw_FrameContext(self, frame):
+        """
+        Draw in the context of a Henderson style frame
+
+        TBD
+        """
+        pass
+
+
+# a list of Segments making up the squarelimit fish
+# uncolored
+
+fish = [
+    # torso outline
+    Segment(2,( (0,0), (20,20), (30,16), (38,12), (44,4),
+                (50,4), (60,4), (72,2), (80,0), (75,3),
+                (68,8), (63,13), (60,16), (53,15), (46,17),
+                (40,20), (32,32), (40,40), (40,60), (33,63),
+                (27,65), (20,64), (17,67), (12,72), (5,77),
+                (0,80), (-2,72), (-4,60), (-4,50), (-4,44),
+                (-12,38), (-16,30), (-20,20), (0,0) )),
+
+    # outer eye
+    Segment(2, ( (0,64), (0,54), (4,58), (0,64) )),
+
+    # inner eye
+    Segment(2, ( (8,68), (8,58), (12,60), (8,68) )),
+
+    # fin line
+    Segment(2, ( (8,54), (16,42), (28,26), (40,16), (58,10) )),
+
+    # flossenrand torso aussen
+    Segment(2, ( (-4,44), (6,28), (20,20) )),
+
+    # fl 1
+    Segment(2, ( (-2,36), (-8,30), (-12,22) )),
+
+    # fl 2
+    Segment(2, ( (2,30), (-6,22), (-8,16) )),
+
+    # fl 3
+    Segment(2, ( (8,24), (-2,16), (-4,10) )),
+
+    # fl 4
+    Segment(2, ( (10,18), (2,10), (0,6) )),
+
+    # flossenrand torso innen
+    Segment(2, ( (20,64), (24,56), (26,44), (32,32) )),
+
+    # fl 1
+    Segment(2, ( (26,56), (30,58), (34,58), (40,54) )),
+
+    # fl 2
+    Segment(2, ( (28,50), (32,52), (36,52), (40,50) )),
+
+    # fl 3
+    Segment(2, ( (30,42), (34,46), (40,46) )),
+
+    # shadow line 1
+    Segment(2, ( (38,36), (40,34) )),
+
+    # shadow line 2
+    Segment(2, ( (38,32), (40,30) )),
+
+    # shadow line 3
+    Segment(2, ( (36,30), (40,26) ))
+]
+
+insettriangle = [
+    # inset triangle
+    Segment(2, ( (10, 10), (70, 10), (10,70), (10,10) ))
+            ]
+
+triangle = [
+    # triangle
+    Segment(2, ( (0, 0), (80, 0), (0, 80), (0, 0) ))
+            ]
+
+
+def build_up_module(picture, doleft, doright, level):
+    """
+    Squarelimit layout engine.
+
+    Assumes a current point and orientation.
+
+    The "up_module" in ascii speak:
+
+    ------+----------+------
+    \     |    /\    |     /
+     \    |   /  \   |    /
+      \   |  /    \  |   /
+       \  | /      \ |  /
+        \ |/        \| /
+         \|----------|/
+
+    The left and right triangles are drawn if doleft/doright are True respectively.
+    """
+    pict_size_x = picture.size[0]
+    pict_size_y = picture.size[1]
+
+    # function scope push
+    push()
+
+    translate(pict_size_x / 2, pict_size_y / 2)
+
+    if dbg_offset:
+        translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+    # pict 1
+    rotate(135)
+    scale(-smaller, smaller)
+    draw_squarellimit_quarter(picture, level, False)
+
+    if True:
+        # right quarter sized pictures
+        push()
+        translate(pict_size_x / 2, pict_size_y / 2)
+
+        if dbg_offset:
+            translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+        rotate(135)
+        scale(-smaller, smaller)
+        draw_squarellimit_quarter(picture, level, True, doleft=False, doright=False)
+        rotate(90)
+        draw_squarellimit_quarter(picture, level, False)
+
+        # right edge
+        if doright:
+            push()
+            translate(pict_size_x, pict_size_y)
+
+            if dbg_offset:
+                translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+            rotate(180)
+            draw_squarellimit_quarter(picture, level, False)
+            rotate(90)
+
+            # this is right outmost
+            draw_squarellimit_quarter(picture, level, True, doleft=False, doright=True)
+            pop()
+        pop()
+
+    rotate(90)
+    draw_squarellimit_quarter(picture, level, False)
+
+    if True:
+        # left quarter sized pictures
+        push()
+        translate(pict_size_x / 2, pict_size_y / 2)
+
+        if dbg_offset:
+            translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+        rotate(135)
+        scale(-smaller, smaller)
+        draw_squarellimit_quarter(picture, level, False)
+
+        # left edge
+        if doleft:
+            push()
+            translate(pict_size_x, pict_size_y)
+
+            if dbg_offset:
+                translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+            rotate(180)
+            draw_squarellimit_quarter(picture, level, False)
+            rotate(-90)
+
+            # this is left outmost
+            draw_squarellimit_quarter(picture, level, True, doleft=True, doright=False)
+            pop()
+        rotate(90)
+        draw_squarellimit_quarter(picture, level, True)
+        pop()
+    pop()
+
+def build_down_module(picture, doleft, doright, level):
+    """
+    Squarelimit layout engine.
+
+    Assumes a current point and orientation.
+
+    The "up_module" in ascii speak:
+
+    ------+----------+------
+    \     |    /\    |     /
+     \    |   /  \   |    /
+      \   |  /    \  |   /
+       \  | /      \ |  /
+        \ |/        \| /
+         \|----------|/
+
+    The left and right triangles are drawn if doleft/doright are True respectively.
+    """
+    pict_size_x = picture.size[0]
+    pict_size_y = picture.size[1]
+
+    # function scope push
+    push()
+
+    translate(pict_size_x / 2, pict_size_y / 2)
+
+    if dbg_offset:
+        translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+    # pict 1
+    rotate(135)
+    scale(-smaller, smaller)
+    draw_squarellimit_quarter(picture, level, False)
+
+    if True:
+        # right quarter sized pictures
+        push()
+        translate(pict_size_x / 2, pict_size_y / 2)
+
+        if dbg_offset:
+            translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+        rotate(135)
+        scale(-smaller, smaller)
+        draw_squarellimit_quarter(picture, level, True, doleft=False, doright=False)
+        rotate(90)
+        draw_squarellimit_quarter(picture, level, False)
+
+        # right edge
+        if doright:
+            push()
+            translate(pict_size_x, pict_size_y)
+
+            if dbg_offset:
+                translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+            rotate(180)
+            draw_squarellimit_quarter(picture, level, False)
+            rotate(90)
+
+            # this is right outmost
+            draw_squarellimit_quarter(picture, level, True, doleft=False, doright=True)
+            pop()
+        pop()
+
+    rotate(90)
+    draw_squarellimit_quarter(picture, level, False)
+
+    if True:
+        # left quarter sized pictures
+        push()
+        translate(pict_size_x / 2, pict_size_y / 2)
+
+        if dbg_offset:
+            translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+        rotate(135)
+        scale(-smaller, smaller)
+        draw_squarellimit_quarter(picture, level, False)
+
+        # left edge
+        if doleft:
+            push()
+            translate(pict_size_x, pict_size_y)
+
+            if dbg_offset:
+                translate(pict_size_x / offsetpart, pict_size_y / offsetpart)
+
+            rotate(180)
+            draw_squarellimit_quarter(picture, level, False)
+            rotate(-90)
+
+            # this is left outmost
+            draw_squarellimit_quarter(picture, level, True, doleft=True, doright=False)
+            pop()
+        rotate(90)
+        draw_squarellimit_quarter(picture, level, True)
+        pop()
+    pop()
+
+
+def draw_squarellimit_quarter(picture, level, buildup, doleft=False, doright=False):
+    """
+    """
+    global no_of_pictures
+    # dbg_frame = True
+    if level >= 0:
+        push()
+        # this global is just for picture counting
+        no_of_pictures += 1
+        picture.draw()
+
+        if buildup:
+            build_up_module(picture, doleft, doright, level - 1)
+        pop()
+
+
+def inversesquarelimit( picture, depth ):
+    """Squarelimit is drawn in a triangular style. For the whole picture, 4 triangles forming
+    a square are drawn.
+    """
+
+    # no of triangles
+    n = 4
+
+    # advance in degrees
+    q = 90
+
+    transform(CORNER)
+    scale(1, 1)
+
+    for i in range(n):
+        push()
+        rotate( i * q )
+
+        if dbg_offset:
+            translate(picture.size[0] / offsetpart, picture.size[1] / offsetpart)
+
+        draw_squarellimit_quarter(picture, depth, True, True, True)
+        pop()
+
+    
+    
+
+def squarelimit(picture, depth, qstart=1, qstop=4):
+    """Squarelimit is drawn in a triangular style. For the whole picture, 4 triangles forming
+    a square are drawn.
+    """
+
+    # no of quarters
+    n = 4
+
+    # advance in degrees
+    q = 90
+
+    transform(CORNER)
+    scale(1, 1)
+
+    for i in range(n):
+        push()
+        rotate( i * q )
+
+        if dbg_offset:
+            translate(picture.size[0] / offsetpart, picture.size[1] / offsetpart)
+
+        draw_squarellimit_quarter(picture, depth, True, True, True)
+        pop()
+
+
+if __name__ == '__builtin__':
+
+    #starting point
+    px = py = 350
+
+    if dbg_offset:
+        parts = 1 + 1 / offsetpart
+        px *= parts
+        py *= parts
+
+    # correct version:
+    fishpicture = Picture( (80, 80), fish)
+    insettrianglepicture = Picture( (80, 80), insettriangle)
+    trianglepicture = Picture( (80, 80), triangle)
+    reset()
+    # scale(2)
+    strokewidth(1)
+    stroke( line_color )
+
+    translate(px, py)
+    rotate(135)
+
+    nofill()
+    scale(2)
+
+    # The actual call:
+    squarelimit(fishpicture, 3)
+    # squarelimit(insettrianglepicture, 8)
+
+    print "#pictures rendered:", no_of_pictures
